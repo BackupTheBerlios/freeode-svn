f72a780b32e996b5211293c52664a4c507eea6b8
eike <eike@28029125-6908-0410-ad1b-aa592d03a77d>
2007-02-26 19:02:43 +0000

    created release tag

---
+++ tags/release-0.3.1/freeode_py/freeode/simlcompiler.py
@@ -0,0 +1,199 @@
+############################################################################
+#    Copyright (C) 2006 by Eike Welk                                       #
+#    eike.welk@post.rwth-aachen.de                                         #
+#                                                                          #
+#    This program is free software; you can redistribute it and#or modify  #
+#    it under the terms of the GNU General Public License as published by  #
+#    the Free Software Foundation; either version 2 of the License, or     #
+#    (at your option) any later version.                                   #
+#                                                                          #
+#    This program is distributed in the hope that it will be useful,       #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#    GNU General Public License for more details.                          #
+#                                                                          #
+#    You should have received a copy of the GNU General Public License     #
+#    along with this program; if not, write to the                         #
+#    Free Software Foundation, Inc.,                                       #
+#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+############################################################################
+
+'''
+Compiler main loop with hight level functions for parsing command line
+options, compiling and running the compiled program.
+'''
+
+
+#import pdb
+import optparse
+import sys
+import os
+import stat
+from subprocess import Popen #, PIPE, STDOUT
+import pyparsing
+import freeode.simlparser as simlparser
+import freeode.pygenerator as pygenerator
+import freeode.ast as ast  #for ast.progVersion
+from freeode.ast import UserException
+
+
+class SimlCompilerMain(object):
+    def __init__(self):
+        super(SimlCompilerMain, self).__init__()
+        self.inputFileName = ''
+        self.outputFileName = ''
+        #which process should be run after compiling
+        self.runSimulation = None #can be: None, 'all', '0', '1', ...
+
+
+    def parseCmdLine(self):
+        '''Parse the command line, and find out what the user wants from us.'''
+        #set up parser for the command line aruments
+        optPars = optparse.OptionParser(
+                    usage='%prog <input_file> [-o <output_file>] [<options>]',
+                    description='Compiler for the Siml simulation language.',
+                    version='%prog ' + ast.progVersion)
+
+        optPars.add_option('-o', '--outfile', dest='outfile',
+                           help='explicitly specify name of output file',
+                           metavar='<output_file>')
+        optPars.add_option('-r', '--run', dest='runone',
+                           help='run generated simulation program after '
+                              + 'compiling. Specify which process to simulate '
+                              + 'with number or use special value "all" '
+                              + '(number counts from top).',
+                           metavar='<number>')
+#        optPars.add_option('--runall', dest='runall',
+#                           action="store_true", default=False,
+#                           help='run all simulation processes after compiling')
+        #do the parsing
+        (options, args) = optPars.parse_args()
+
+        #get name of input file
+        #self.inputFileName = None
+        if len(args) > 0:
+            self.inputFileName = args[0]
+        if not self.inputFileName:
+            optPars.error('no input file given')
+        #split input file name into basename and extension
+        nameParts = self.inputFileName.rsplit('.',1)
+        if len(nameParts)  > 1:
+            inputFileExtension = nameParts[1]
+        else:
+            inputFileExtension = '' #filename contained no '.' char
+        inputFileBaseName = nameParts[0]
+
+        #see if file extension is good
+        if inputFileExtension.lower() != 'siml':
+            print 'warning: programs in the SIML language ' + \
+                  'should have the extension ".siml"'
+
+        #get name of output file
+        if options.outfile:
+            #output file name is explicitly given
+            self.outputFileName = options.outfile
+        else:
+            #use inputFileName but replace extension with '.py'
+            self.outputFileName = inputFileBaseName + '.py'
+
+        #see if user whishes to run the simulation after compiling
+        if options.runone == 'all':
+            self.runSimulation = 'all'
+        elif options.runone: #anythin else is considered a number
+            #test if argument is a number
+            try:
+                int(options.runone)
+            except:
+                optPars.error('option "-r": argument must be number or "all". I got: %s'
+                              % options.runone)
+            #convert into string containing a number
+            self.runSimulation = str(int(options.runone))
+#        #special option runall
+#        if options.runall:
+#            self.runSimulation = 'all'
+
+
+    def doCompile(self):
+        '''Do the work'''
+        #create the top level objects that do the compilation
+        parser = simlparser.ParseStage()
+        iltGen = simlparser.ILTGenerator()
+        progGen = pygenerator.ProgramGenerator()
+
+        #the compilation proper
+        try:
+            astTree = parser.parseProgramFile(self.inputFileName)
+            #print astTree
+            iltTree = iltGen.createIntermediateTree(astTree)
+            #print iltTree
+            progGen.createProgram(iltTree)
+            progStr = progGen.buffer()
+        #errors from freeode
+        except UserException, theError:
+            print >> sys.stderr, theError
+            sys.exit(1)
+        #errors from pyparsing. Don't remove: parser may re-raise pyparsing errors.
+        except pyparsing.ParseException, theError:
+            print >> sys.stderr, 'syntax error: ', theError
+            sys.exit(1)
+
+        #write generated program to file
+        try:
+            outputFile = open(self.outputFileName,'w')
+            outputFile.write(progStr)
+            outputFile.close()
+            #make genrated program executable
+            modeBits = os.stat(self.outputFileName).st_mode
+            os.chmod(self.outputFileName, modeBits | stat.S_IEXEC)
+        except IOError, theError:
+            print >> sys.stderr, 'error: could nor write output file\n', theError
+            sys.exit(1)
+
+        print 'Compilation finished successfully.'
+        #print 'input file: %s, output file: %s' % (self.inputFileName, self.outputFileName)
+
+
+    def runProgram(self):
+        '''Run the generated program if the user wants it.'''
+        if self.runSimulation == None:
+            return
+
+        #optStr = ' -r %s' % self.runSimulation
+        cmdStr = 'python %s -r %s --prepend-newline' % (self.outputFileName,
+                                                        self.runSimulation)
+        proc = Popen([cmdStr], shell=True, #bufsize=1000,
+                     stdin=None, stdout=None, stderr=None, close_fds=True)
+        print 'running generated program. PID: %d' % proc.pid
+
+
+    def mainFunc(self):
+        '''the main function'''
+        try:
+            self.parseCmdLine()
+            self.doCompile()
+            self.runProgram()
+        except SystemExit:
+            raise #for sys.exit() - the error message was already printed
+        except Exception: #Any othe exception must be an internal error
+            print >> sys.stderr, \
+                  '\nOh my golly! Compiler internal error! \n\n', \
+                  'Please file a bug report at the project\'s website,', \
+                  'or send an e-mail \n', \
+                  'with with a bug report to the developer(s).\n', \
+                  'The bug report should include the traceback', \
+                  'at the end of this message. \n', \
+                  'Please include also a short description of the error. \n', \
+                  'Website: \n', \
+                  '  https://developer.berlios.de/projects/freeode/\n', \
+                  'E-mail: \n', \
+                  '  eike@users.berlios.de \n\n', \
+                  'SIML compiler version: %s \n' %  ast.progVersion
+            raise #gets traceback and ends program
+        #return with success
+        sys.exit(0)
+
+
+if __name__ == '__main__':
+    #do self tests
+    #TODO: try to compile all example models as unit test
+    pass
---
+++ tags/release-0.3.1/website/development.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+  <title>Free ODE</title>
+  <meta name="GENERATOR" content="Quanta Plus">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="KEYWORDS" content="freeode, simulation, numerical math, python, ODE, differential equations">
+  <META name="Author" content="Eike Welk">
+  <link rel="stylesheet" type="text/css" href="freeode.css">
+</head>
+
+
+<body>
+<!--Title-->
+<?php if( file_exists("header.html") ){ include("header.html"); } ?>
+
+<!--Navigation-->
+<?php if( file_exists("menu-left.html") ){ include("menu-left.html"); } ?>
+
+
+<div id="navigation_page">
+  <ul>
+    <li>Installation</li>
+    <li><A href="development.php#top">Top</A></li>
+    <li><A href="development.php#get-the-sources">Get the Sources</A></li>
+  </ul>
+</div>
+
+
+<div id="contents">
+
+  <A name="top"></A>
+  <h1>Development</h1>
+  <p>
+    The compiler is a Python program that uses the
+    <A href="http://pyparsing.wikispaces.com/">Pyparsing</A>
+    library for parsing.
+    The generated Python program uses the
+    <A href="http://numpy.scipy.org/">NumPy</A>
+    ,
+    <A href="http://www.scipy.org/">SciPy</A>
+    and
+    <A href="http://matplotlib.sourceforge.net/">Matplotlib</A>
+    libraries for numerical computations and plotting.
+    For a detailed discussion of the dependencies see the
+    <A href="installation.php">installation</A>
+    instructions.
+  </p>
+
+  <p>
+    Python promises to be highly platform independent.
+    Therfore everything should work on Windows exacly as it works on Linux.
+  </p>
+
+  <p>
+    Development currently happens on Linux only.
+    The
+    <A href="http://pydev.sourceforge.net/">Pydev</A>
+    extension for
+    <A href="http://www.eclipse.org/">Eclipse</A>
+    is used as the IDE. Pydev analyzes the program and can complete Python code much
+    better than any other free IDE I know. It uses
+    <A href="www.logilab.org/857">Pylint</A>
+    to flag errors and bad coding practice.
+    Eclipse and Pydev are written in Java,
+    and should therfore work on Windows
+    as well.
+  </p>
+  <p>
+    Pydev's code completion is quite slow;
+    it can unfortunately not usefully work when all of NumPy
+    has been imported. It will then always hang for ~30s before it can show a list of
+    possible completions (on Pentium M 1.4 GHz).
+<!--After typing: 'from scipy inport *' it will pause for about half a minute-->
+<!--when you type a dot.-->
+    Pydev's slowness is the reason why the code conains lines like this:
+    <div id="code">
+      <pre>from numpy import array, linspace, zeros, shape, ones, resize</pre>
+    </div>
+    instead of:
+    <div id="code">
+      <pre>from numpy import *</pre>
+    </div>
+  </p>
+
+
+  <A name="get-the-sources">
+  </A><h2>Getting the sources</h2>
+  <p>
+    The latest version of all of the project's files, is available from the subversion
+    repository at BerliOS.
+  </p>
+  <p>
+    To check out (download) only the Python code, type the following in a shell window:
+  </p>
+  <div id="code">
+    <pre>svn checkout svn://svn.berlios.de/freeode/trunk/freeode_py </pre>
+  </div>
+
+  <p>
+    To check out everything, including website, and some additional documentation, do:
+  </p>
+  <div id="code">
+    <pre>svn checkout svn://svn.berlios.de/freeode/trunk</pre>
+  </div>
+
+  </div>
+
+
+</div>
+
+<!--footer with the required berlios image-->
+<?php if( file_exists("footer.html") ){ include("footer.html"); } ?>
+
+</body>
+</html>
